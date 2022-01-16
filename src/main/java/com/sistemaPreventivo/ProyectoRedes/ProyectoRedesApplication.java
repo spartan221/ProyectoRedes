5f856a53b846cc460d586c4cc419e0d635bb60d9
spartan221 <imorales@unal.edu.co>
2022-01-16 15:18:44 -0500

    Creacion del proyecto con SpringBoot

---
+++ src/main/java/com/sistemaPreventivo/ProyectoRedes/ProyectoRedesApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaPreventivo.ProyectoRedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRedesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaPreventivo/ProyectoRedes/ProyectoRedesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaPreventivo.ProyectoRedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRedesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
